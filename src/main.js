647276a9681003c3c51473712df50164a130c110
Dima Shchelko <dimashchelko@Dimas-MacBook-Pro.local>
2020-01-28 15:15:04 +0200

    add project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD4-7hXdMCxVcg8quf765lIsgjwpJA5_20",
+  authDomain: "vue-crm-63e25.firebaseapp.com",
+  databaseURL: "https://vue-crm-63e25.firebaseio.com",
+  projectId: "vue-crm-63e25",
+  storageBucket: "vue-crm-63e25.appspot.com",
+  messagingSenderId: "1082466268622",
+  appId: "1:1082466268622:web:ff810165325da389fd279f"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
---
+++ src/views/Categories.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Категории</h3>
+    </div>
+    <section>
+      <div class="row">
+        <CategoryCreate @created="addNewCategory"></CategoryCreate>
+        <CategoryEdit></CategoryEdit>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import CategoryCreate from '../components/CategoryCreate'
+import CategoryEdit from '../components/CategoryEdit'
+export default {
+  name: 'categories',
+  data: () => ({
+    categories: []
+  }),
+  components: {
+    CategoryCreate, CategoryEdit
+  },
+  methods: {
+    addNewCategory (category) {
+      this.categories.push(category)
+      console.log(this.categories)
+    }
+  }
+}
+</script>
